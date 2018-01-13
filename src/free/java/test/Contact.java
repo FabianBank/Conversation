644fc3400e480442e6c8092b7514c19a974b258a
Fabian Bank <fabian.bank@hva.nl>
2018-01-13 16:50:01 +0100

    Enabled multi dex to solve max method problem

--- src/free/java/test/Contact.java
+++ src/free/java/test/Contact.java
@@ -1,9 +1,19 @@
 package test;
 
+import org.testng.annotations.Test;
+
 /**
- * Created by fabia on 1/13/2018.
+ * Created by fabian on 1/3/2017.
  */
 
 public class Contact {
 
-}
+    @Test
+    public void addContact(){
+
+//        Contact contact = new Contact(firstName, lastName);
+//        assertEquals(contact.getName(), “name”);
+//        assertEquals(contact.getLastName(), “lastName”);
+//    }
+
+}}
