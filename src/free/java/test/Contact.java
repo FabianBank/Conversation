5a9fb9e82fcd68a432c04ff36f80663a710e9c41
Fabian Bank <fabian.bank@hva.nl>
2018-01-14 14:45:44 +0100

    test

--- src/free/java/test/Contact.java
+++ src/free/java/test/Contact.java
@@ -2,18 +2,26 @@ package test;
 
 import org.testng.annotations.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by fabian on 1/3/2017.
  */
 
 public class Contact {
 
+    private static String firstName = "Fabian";
+    private static String lastName = "Bank";
+
+
+
     @Test
     public void addContact(){
 
-//        Contact contact = new Contact(firstName, lastName);
-//        assertEquals(contact.getName(), “name”);
-//        assertEquals(contact.getLastName(), “lastName”);
+        Contact contact = new Contact();
+
+        assertEquals(contact.getName(), “Fabian”);
+        assertEquals(contact.getLastName(), “Bank”);
 //    }
 
 }}
