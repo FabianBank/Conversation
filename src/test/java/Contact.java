5f979537c3a8b189acffe0b1877d2e19e697d95f
Fabian Bank <fabian.bank@hva.nl>
2018-01-14 17:57:30 +0100

    gradle fixes

---
+++ src/test/java/Contact.java
@@ -0,0 +1,52 @@
+
+import org.junit.Test;
+
+import eu.siacs.conversations.entities.Conversation;
+import eu.siacs.conversations.entities.Message;
+import eu.siacs.conversations.xmpp.jid.InvalidJidException;
+import eu.siacs.conversations.xmpp.jid.Jid;
+
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by fabian on 1/3/2017.
+ */
+
+public class Contact {
+
+    private static String firstName = "Fabian";
+    private static String lastName = "Bank";
+
+    @Test
+    public void messageBodyTest() throws InvalidJidException {
+        Message message;
+
+//        Conversation conversation = new Conversation("test", "test", "test",
+//                "test", new Jid("jid", true), 1, 1, 1, "test");
+
+//        message = new Message(conversation, "Dit is de body van het bericht", 1);
+//        assertEquals(message.getBody(), "Dit is de body van het bericht");
+    }
+
+
+    @Test
+    public void addContact() {
+
+        Contact contact = new Contact();
+
+        assertEquals(contact.getClass().getName(), "fabian");
+
+
+    }
+
+    @Test
+    public void startGroupConversation() throws InvalidJidException {
+//        Conversation conversation = new Conversation("test", "test", "test",
+//                "test", new Jid("jid", true), 1, 1, 1, "test");
+
+//        assertEquals(conversation.getName(), “test”);
+
+    }
+
+}
